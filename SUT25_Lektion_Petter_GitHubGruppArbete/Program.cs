0954fa4b81cafdc679f7d79bba74e6aafa6dfaad
nikki norberg <nikki.norberg@hotmail.com>
2025-09-24 14:26:16 +0200

    added multiplication function

--- SUT25_Lektion_Petter_GitHubGruppArbete/Program.cs
+++ SUT25_Lektion_Petter_GitHubGruppArbete/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +13,10 @@ namespace SUT25_Lektion_Petter_GitHubGruppArbete
         {
 
         }
+        static int Multiplication(int one, int two)
+        {
+            return one * two;
+        }
     }
+
 }
